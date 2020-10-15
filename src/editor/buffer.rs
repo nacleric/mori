f8499cca8f6361f3a108ab06a02dc9c676184c87
Eric Ng <naclericny@gmail.com>
2020-10-14 22:14:51 -0400

    finished update test, working on delete test

--- src/editor/buffer.rs
+++ src/editor/buffer.rs
@@ -3,17 +3,44 @@ mod unit_tests;
 use crate::error::Result;
 
 #[derive(Debug, Default, Eq, PartialEq)]
+pub struct Position {
+    col: u32, // x-coord
+    row: u32, // y-coord
+}
+
+impl Position {
+    pub fn new() -> Self {
+        Self { col: 0, row: 0 }
+    }
+
+    pub fn update(&mut self, col: u32, row: u32) {
+        self.col = col;
+        self.row = row;
+    }
+}
+
+#[derive(Debug, Default, Eq, PartialEq)]
 pub struct Buffer {
     data: String,
+    pos: Position,
 }
 
 impl Buffer {
     pub fn new() -> Self {
-        Self { data: String::new() }
+        Self { data: String::new(), pos: Position::new() }
     }
 
     pub fn insert_glyph(&mut self, glyph: char) -> Result<&mut Self> {
         self.data.push(glyph);
+        Ok(self)
+    }
+
+    // Note: https://stackoverflow.com/questions/24542115/how-to-index-a-string-in-rust
+    // TODO: Might need to account for glyphs that take up 2+ characters
+    pub fn delete_glyph(&mut self) -> Result<&mut Self> {
+        let data = self.data.chars().nth(self.pos.col as usize).unwrap();
+        // let data = self.data.chars();
+        self.pos.col -= 1;
         Ok(self)
     }
 
--- src/editor/buffer/unit_tests.rs
+++ src/editor/buffer/unit_tests.rs
@@ -15,3 +15,65 @@ fn insert_glyph__adding_a_glyph_to_an_empty_data_model_yields_a_model_containing
     assert_eq!(res?.contents(), expected_result);
     Ok(())
 }
+
+#[test]
+fn insert_escape_character__adding_an_escape_character_glyph_to_an_empty_data_model() -> Result<()> {
+    // Given
+    let escape_character_glyph = '\n';
+    let expected_result = String::from(escape_character_glyph);
+    let mut sut = Buffer::new();
+
+    // When
+    let res = sut.insert_glyph(escape_character_glyph);
+
+    // Then
+    assert_eq!(res?.contents(), expected_result);
+    Ok(())
+}
+
+#[test]
+fn insert_tab___adding_a_tab_to_an_empty_data_model() -> Result<()> {
+    // Given
+    let escape_character_glyph = '\t';
+    let expected_result = String::from(escape_character_glyph);
+    let mut sut = Buffer::new();
+
+    // When
+    let res = sut.insert_glyph(escape_character_glyph);
+
+    // Then
+    assert_eq!(res?.contents(), expected_result);
+    Ok(())
+}
+
+#[test]
+fn update_position__update_position_within_empty_data_model() {
+    // Given
+    let current_position = Buffer::new().pos;
+    let expected_result = Position { col: 6, row: 0 };
+    let mut sut = current_position;
+
+    // When
+    sut.update(6, 0);
+    let res = sut;
+
+    // Then
+    assert_eq!(res, expected_result);
+}
+
+// Doesn't work yet
+#[test]
+fn delete_glyph__deleting_a_glyph_from_experimental_data_model() -> Result<()> {
+    // Given
+    let sentence = String::from("helloo world");
+    let expected_result = String::from("hello world");
+    let mut sut = Buffer::new();
+
+    // When
+    sut.pos.update(6, 0);
+    let res = sut.delete_glyph();
+
+    // Then
+    assert_eq!(res, expected_result);
+    Ok(())
+}
